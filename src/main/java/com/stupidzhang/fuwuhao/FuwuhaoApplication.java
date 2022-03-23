2cb4e6f99517a0568c3a23ba9c0e13075393a2a6
张胜军 <wb.zhangshengjun@mesg.corp.netease.com>
2022-03-23 14:12:03 +0800

    feat: init

---
+++ src/main/java/com/stupidzhang/fuwuhao/FuwuhaoApplication.java
@@ -0,0 +1,16 @@
+package com.stupidzhang.fuwuhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 张胜军
+ * @date 2022/3/23 14:14
+ */
+@SpringBootApplication
+public class FuwuhaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuwuhaoApplication.class, args);
+    }
+}
